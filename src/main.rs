a70546e0550bb8de63795ce234b2d5202a6859f0
Brandon Ros <brandonros1@gmail.com>
2025-02-22 09:51:48 -0500

    works

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 #![no_std]
 #![no_main]
 
+use bt_hci::controller::ExternalController;
 use cyw43_pio::{PioSpi, RM2_CLOCK_DIVIDER};
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_rp::bind_interrupts;
+use embassy_rp::gpio::{Level, Output};
 use embassy_rp::peripherals::{DMA_CH0, PIO0, UART1};
 use embassy_rp::pio::{self, Pio};
-use embassy_rp::{bind_interrupts, gpio, uart};
-use embassy_time::{Duration, Timer};
-use gpio::{Level, Output};
+use embassy_rp::uart::{self};
 use static_cell::StaticCell;
+use trouble_example_apps::ble_bas_peripheral;
 use {defmt_serial as _, panic_probe as _};
 
 // Program metadata for `picotool info`.
@@ -41,8 +43,19 @@ async fn cyw43_task(
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
+
+    let uart1 = UART.init(uart::Uart::new_blocking(
+        p.UART1,
+        p.PIN_4, // tx, blue, goes to rx
+        p.PIN_5, // rx, white, goes to tx
+        uart::Config::default(),
+    ));
+
+    defmt_serial::defmt_serial(uart1);
+
     let fw = include_bytes!("../cyw43-firmware/43439A0.bin");
     let clm = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
+    let btfw = include_bytes!("../cyw43-firmware/43439A0_btfw.bin");
 
     let pwr = Output::new(p.PIN_23, Level::Low);
     let cs = Output::new(p.PIN_25, Level::High);
@@ -60,31 +73,12 @@ async fn main(spawner: Spawner) {
 
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
     let state = STATE.init(cyw43::State::new());
-    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    let (_net_device, bt_device, mut control, runner) =
+        cyw43::new_with_bluetooth(state, pwr, spi, fw, btfw).await;
     unwrap!(spawner.spawn(cyw43_task(runner)));
-
     control.init(clm).await;
-    control
-        .set_power_management(cyw43::PowerManagementMode::PowerSave)
-        .await;
 
-    let uart1 = UART.init(uart::Uart::new_blocking(
-        p.UART1,
-        p.PIN_4, // tx, blue, goes to rx
-        p.PIN_5, // rx, white, goes to tx
-        uart::Config::default(),
-    ));
+    let controller: ExternalController<_, 10> = ExternalController::new(bt_device);
 
-    defmt_serial::defmt_serial(uart1);
-
-    let delay = Duration::from_secs(1);
-    loop {
-        info!("led on!");
-        control.gpio_set(0, true).await;
-        Timer::after(delay).await;
-
-        info!("led off!");
-        control.gpio_set(0, false).await;
-        Timer::after(delay).await;
-    }
+    ble_bas_peripheral::run::<_, 128>(controller).await;
 }
